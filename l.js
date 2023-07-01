bd36ec2ef1b261a1d9ee913da81f7921969e01c2
Pradeep Singh <133224740+pradeepsinghs@users.noreply.github.com>
2023-07-01 19:28:04 +0530

    Add files via upload

---
+++ l.js
@@ -0,0 +1,163 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of these is not a core data type?",
+    answer: "Class",
+    options: [
+      "Tuple",
+      "Class",
+      "Dictionary",
+      "List"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is a Python tuple?",
+    answer: "(1,2,3,4)",
+    options: [
+      "{}",
+      "{1,2,3,4}",
+      "(1,2,3,4)",
+      "[1,2,3,4]"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+   {
+    numb: 6,
+    question: "What is a correct syntax to output 'Hello World' in Python?",
+    answer: "Print('hello world')",
+    options: [
+      "Print('hello world')",
+      "Print('hello world');",
+      "Printer('hello world');",
+      "Printing('hello world');"
+    ]
+  },
+
+    {
+    numb: 7,
+    question: "What property changes the inside space of the container?",
+    answer: "padding",
+    options: [
+      "border",
+      "padding",
+      "All are correct",
+      "margin"
+    ]
+  },
+
+    {
+    numb: 8,
+    question: "If we want to show an Arrow as cursor, then which value we will use?",
+    answer: "default",
+    options: [
+      "pointer",
+      "default",
+      "arrow",
+      "arr"
+    ]
+  },
+
+    {
+    numb: 9,
+    question: "How can you make a bulleted list?",
+    answer: "ul",
+    options: [
+      "list",
+      "ol",
+      "ul",
+      "dl"
+    ]
+  },
+
+    {
+    numb: 10,
+    question: "What command is used to upload your code and changes to GitHub?",
+    answer: "$ git push",
+    options: [
+      "$ git push",
+      "$ git upload",
+      "$ git add",
+      "$ git status"
+    ]
+  },
+
+    {
+    numb: 11,
+    question: "How do I add files to a commit?",
+    answer: "$ git add",
+    options: [
+      "$ git stage",
+      "$ git add",
+      "$ git reset",
+      "$ git stage"
+    ]
+  },
+
+    {
+    numb: 12,
+    question: "Which language is used to enhance a webpage, e.g. change background colour?",
+    answer: "CSS",
+    options: [
+      "JavaScript",
+      "SQL",
+      "HTML",
+      "CSS"
+    ]
+  },
+
+    {
+    numb: 13,
+    question: "Audio tag is used for audio in HTML5?",
+    answer: " True",
+    options: [
+      "False",
+      "True",
+    
+    ]
+  },
+
+    {
+    numb: 14,
+    question: "How can you create a newline in a print function?",
+    answer: "n",
+    options: [
+      "n",
+      "new",
+      "newline",
+      "printline"
+    ]
+  },
+];
